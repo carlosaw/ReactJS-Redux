3936cbecfe09e5c2d8032c45fb921f2bd4c7df74
carlosaw <carlosfreevg@gmail.com>
2023-08-31 16:13:50 -0400

    first

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Meu nome é: ... e tenho ... anos.<br/>
+      Tema: ...
+
+      <hr/>
+      <input type='text' value={'...'} />
+
+      <hr/>
+      <button>Switch Theme</button>
+    </div>
+  );
+}
+
+export default App;
